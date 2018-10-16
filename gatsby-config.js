610e175fa158c6695ed44314833a870079f834de
Alex Chan <achan@trifacta.com>
2018-10-15 21:39:56 -0700

    Minimal use case

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,16 +6,12 @@ module.exports = {
     {
       resolve: 'gatsby-mdx',
       options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
         mdPlugins: [],
         hastPlugins: [],
         gatsbyRemarkPlugins: [],
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'posts',
-        path: `${__dirname}/content/`,
       },
     },
   ],
